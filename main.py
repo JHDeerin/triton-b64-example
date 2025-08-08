32fdab98cd13a32ee812624773e35598b3b8fed3
Jake Deerin <2016deerinjo@gmail.com>
2025-08-07 21:32:04 -0400

    updated example to work with batching

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import requests
 
 TRITON_URL = "http://localhost:8000/v2/models/image_decode/infer"
 
+
 def encode_image_to_base64(img_path):
     with open(img_path, "rb") as f:
-        return base64.b64encode(f.read()).decode('utf-8')
+        return base64.b64encode(f.read()).decode("utf-8")
+
 
 def send_request(image_path):
     # Encode to base64 string
@@ -18,18 +20,15 @@ def send_request(image_path):
     inputs = [
         {
             "name": "INPUT_IMAGE_B64",
-            "shape": [1],
+            "shape": [1, 1],  # [batch_size, 1] - "1" since only 1 string
             "datatype": "BYTES",
-            "data": [b64_str]
+            "data": [[b64_str]],
         }
     ]
 
-    outputs = [{"name": "OUTPUT_IMAGE_TENSOR"}]
+    outputs = [{"name": "OUTPUT_IMAGE_TENSOR", "name": "OUTPUT_IMAGE_TENSOR"}]
 
-    payload = {
-        "inputs": inputs,
-        "outputs": outputs
-    }
+    payload = {"inputs": inputs, "outputs": outputs}
 
     r = requests.post(TRITON_URL, json=payload)
     r.raise_for_status()
@@ -38,11 +37,12 @@ def send_request(image_path):
     print(result)
 
     # Triton returns outputs in base64 for BYTES or nested lists for numeric tensors
-    raw_data = result['outputs'][0]['data']
-    shape = result['outputs'][0]['shape']
+    raw_data = result["outputs"][0]["data"]
+    shape = result["outputs"][0]["shape"]
     np_array = np.array(raw_data, dtype=np.float32).reshape(shape)
 
     return np_array
+
 
 if __name__ == "__main__":
     img_tensor = send_request("test.jpeg")
--- triton_models/image_decode/1/model.py
+++ triton_models/image_decode/1/model.py
@@ -4,29 +4,60 @@ import numpy as np
 from PIL import Image
 import triton_python_backend_utils as pb_utils
 
+
 class TritonPythonModel:
     def initialize(self, args):
         pass
 
     def execute(self, requests):
         responses = []
-        print(len(requests))
         for request in requests:
-            # Get the input tensor as a numpy object
             in_tensor = pb_utils.get_input_tensor_by_name(request, "INPUT_IMAGE_B64")
-            b64_str = in_tensor.as_numpy()[0].decode('utf-8')  # string from TYPE_STRING
+            # input for TYPE_STRING will be a numpy array with `bytes` inside
+            in_array = in_tensor.as_numpy()
 
-            # Decode base64 -> JPEG bytes
-            img_bytes = base64.b64decode(b64_str)
+            out_tensor = self.batch_response(in_array)
 
-            # Load image via Pillow
-            img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
-
-            # Create output tensor
-            img_array = np.asarray(img, dtype=np.float32)
-            out_tensor = pb_utils.Tensor("OUTPUT_IMAGE_TENSOR", img_array)
-
-            # Build inference response
             responses.append(pb_utils.InferenceResponse(output_tensors=[out_tensor]))
 
         return responses
+
+    def single_response(self, in_array: np.ndarray):
+        """
+        Use when configured max_batch_size = 0.
+
+        Will return a tensor of size [Height, Width, 3].
+        """
+        # Create output tensor
+        img_array = img_array = self._b64_array_to_img_array(in_array)
+        out_tensor = pb_utils.Tensor("OUTPUT_IMAGE_TENSOR", img_array)
+        return out_tensor
+
+    def batch_response(self, in_array: np.ndarray):
+        """
+        Use when configured max_batch_size != 0.
+
+        Will return a tensor of size [N, Height, Width, 3], where
+        N="the number of batches in the request."
+        """
+        images = []
+        for b64_batch in in_array:
+            img_array = self._b64_array_to_img_array(b64_batch)
+            images.append(img_array)
+        all_images_array = np.array(images, dtype=np.float32)
+        out_tensor = pb_utils.Tensor("OUTPUT_IMAGE_TENSOR", all_images_array)
+        return out_tensor
+
+    def _b64_array_to_img_array(self, b64_array: np.ndarray) -> np.ndarray:
+        """
+        Convert an input array w/ a base64 string to an RGB image array.
+
+        Will return an array of size [Height, Width, 3] (i.e. "HWC" order).
+        """
+        b64_str = b64_array[0].decode("utf-8")
+        img_bytes = base64.b64decode(b64_str)
+
+        # Load image and convert it into a numpy array
+        img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
+        img_array = np.asarray(img, dtype=np.float32)
+        return img_array
